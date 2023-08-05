cffc958ec69f7f94fd2970f5d706841ae495e751
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-08-04 20:41:32 -0600

    Space Game: Scoring and Lives

---
+++ 06-Space-game/05-Keeping-score/text.js
@@ -0,0 +1,7 @@
+// Dibuja un texto en la pantalla
+function drawText(message, x, y) {
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "red";
+    ctx.textAlign = "right";
+    ctx.fillText("show this on the screen", 0, 0);
+}
