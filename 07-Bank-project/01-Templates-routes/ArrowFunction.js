d2fc28072c5334db8333a790a0f48d6a9e627c84
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-08-14 19:03:13 -0600

    Bank project: Templates & Routes

---
+++ 07-Bank-project/01-Templates-routes/ArrowFunction.js
@@ -0,0 +1,9 @@
+// Funcion que suma 2 numeros
+const add = (a, b) => a + b;
+console.log(add(1, 2)); // 3
+
+// Funcion que resta 2 numeros
+const subtract = (a, b) => { // Las llaves se pueden utilizar cuando se introducen varias lineas de codigo
+    return a - b
+};
+console.log(subtract(2, 1)); // 1
